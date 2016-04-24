71f44a6d167253e6fd4accdbc47e9d6fe10fcbd1
Damien McGrath <damien.mcgrath7@gmail.com>
2016-04-24 07:25:32 +0000

    adding new conf file

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 var Routes     = require('./app/controllers/routes');
 var Lesson     = require('./app/models/lesson');
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017'); // connect to our database
+//mongoose.connect('mongodb://0.0.0.0:27017'); // connect to our database
+mongoose.connect('mongodb://146.148.62.134:80/golfDB');
+
 // =============================================================================
 
 // call the packages we need
